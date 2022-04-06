2443b0782c4e17a95485564a28f7edbbb3e78e76
Omkar Singh <omkarsingh.ohm@gmail.com>
2022-04-06 22:22:26 +0530

    Base Project
    Base Project

---
+++ JavaRevision/src/main/java/com/java/revision/JavaRevisionApplication.java
@@ -0,0 +1,13 @@
+package com.java.revision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRevisionApplication.class, args);
+	}
+
+}
