8c9bc0c6771c1647087695f401ec63ef1623ec92
Omkar Singh <omkarsingh.ohm@gmail.com>
2022-04-07 23:10:50 +0530

    Import package

--- JavaRevision/src/main/java/com/java/revision/collection/SetTest.java
+++ JavaRevision/src/main/java/com/java/revision/collection/SetTest.java
@@ -1,10 +1,13 @@
 package com.java.revision.collection;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Set<Integer> s=new HashSet();
+		Set<Integer> s=new HashSet<Integer>();
 		s.add(1);
 		s.add(2);
 		s.add(3);
