5ac14091ce8fc716afb47bb4db83d6c08a3dfff5
Omkar Singh <omkarsingh.ohm@gmail.com>
2022-04-07 23:09:36 +0530

    Update SetTest.java

--- JavaRevision/src/main/java/com/java/revision/collection/SetTest.java
+++ JavaRevision/src/main/java/com/java/revision/collection/SetTest.java
@@ -4,6 +4,14 @@ public class SetTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Set<Integer> s=new HashSet();
+		s.add(1);
+		s.add(2);
+		s.add(3);
+		s.add(4);
+		s.add(5);
+		
+		s.stream().forEach((i)->System.out.println(i));
 
 	}
 
