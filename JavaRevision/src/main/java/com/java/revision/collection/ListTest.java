4248483c43190f89381c357f68b04575b2bc5aa0
Omkar Singh <omkarsingh.ohm@gmail.com>
2022-04-07 22:58:40 +0530

    first program

--- JavaRevision/src/main/java/com/java/revision/collection/ListTest.java
+++ JavaRevision/src/main/java/com/java/revision/collection/ListTest.java
@@ -1,10 +1,20 @@
 package com.java.revision.collection;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListTest {
 
 	public static void main(String[] args) {
 
-		System.out.println("Java List : "); 
+		List<Integer> l=new ArrayList();
+		l.add(1);
+		l.add(2);
+		l.add(3);
+		l.add(4);
+		l.add(5);
+		
+		l.stream().forEach((i)->System.out.println(i));
 
 	}
 
