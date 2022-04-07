f83b3dda04c377114d94cfa15d089b874b80a6d7
Omkar Singh <omkarsingh.ohm@gmail.com>
2022-04-07 23:06:42 +0530

    Modification done on it

--- JavaRevision/src/main/java/com/java/revision/collection/PullTest.java
+++ JavaRevision/src/main/java/com/java/revision/collection/PullTest.java
@@ -3,7 +3,7 @@ package com.java.revision.collection;
 public class PullTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("This file comes from github repository");
 
 	}
 
